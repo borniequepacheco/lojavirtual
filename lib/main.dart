470a6874f539617d868c4202f9404f19ce75a007
Bornieque Pacheco <31260523+borniequepacheco@users.noreply.github.com>
2021-07-13 12:20:47 -0300

    Aula 13
    Criando a tela base

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:lojavirtual/screens/base/base_screen.dart';
 
 void main() async {
   runApp(MyApp());
 
-  await Firebase.initializeApp();
+  //main firebase
+  /* void main() async {
+  runApp(MyApp());
+  await Firebase.initializeApp(); */
 
   //Gravação banco
   /* FirebaseFirestore.instance
@@ -50,14 +54,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Loja Virtual',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Container(
-        color: Colors.amber,
-      ),
+      home: BaseScreen(),
     );
   }
 }
